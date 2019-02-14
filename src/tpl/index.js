b45420e56ef942ad7e576df0253ce1717aa03825
张集伟 <zhangjw01@mingyuanyun.com>
2019-02-14 18:45:44 +0800

    "用gulp流处理markdown文件"—咦~这里有一只单身的帅狗在写代码呀！

--- src/tpl/index.js
+++ src/tpl/index.js
@@ -3,7 +3,18 @@ const path = require('path')
 const template = require('art-template');
 
 const absPath = (relativePath) => path.join(__dirname, relativePath)
-const readTpl = (tplPath) => fs.readFileSync(absPath(tplPath), {encoding: 'utf8'})
+const readTpl = (tplPath) => {
+    return {
+        tplText: fs.readFileSync(absPath(tplPath), {encoding: 'utf8'}),
+        wrapper: function(wrapperText) {
+            // 由里到外
+            this.tplText = wrapperText.replace(/(<slot \/>) | (<slot>\s*<\/slot>)/, this.tplText);
+            return this;
+        },
+
+    }
+    
+}
 
 const wrapperTpl = readTpl('./wrapper.art')
 const wrapperAsideTpl = readTpl('./wrapperAside.art')
@@ -21,13 +32,20 @@ function Tpl (pageType) {
         return template.render(this.tplText, {...data, pageType: this.pageType, isDev: process.env.dev})
     }
 }
-
+const wrapper = (wrapperText) => {
+    // 由里到外
+    this.tplText = wrapperText.replace(/(<slot \/>) | (<slot>\s*<\/slot>)/, this.tplText);
+    return this;
+}
 const about = (new Tpl('about')).wrapper(wrapperAsideTpl).wrapper(wrapperTpl)
 const article = (new Tpl('article')).wrapper(wrapperTpl)
 const ideas = (new Tpl('idea')).wrapper(wrapperAsideTpl).wrapper(wrapperTpl)
 const category = (new Tpl('category')).wrapper(wrapperAsideTpl).wrapper(wrapperTpl)
 const articleList = (new Tpl('articleList')).wrapper(wrapperAsideTpl).wrapper(wrapperTpl)
 const links = (new Tpl('link')).wrapper(wrapperAsideTpl).wrapper(wrapperTpl)
+const _about = function() {
+    
+}
 
 module.exports = {
     about, article, ideas, category, links, articleList
