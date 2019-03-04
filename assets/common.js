12e26d115ee678d51ab1ee57f3af9cb91558a8c5
qoxop <1106291186@qq.com>
2019-03-04 23:56:18 +0800

    优化样式、交互

--- assets/common.js
+++ assets/common.js
@@ -1,5 +1,5 @@
 window.exec = (function() {
-    const directHandlers = {
+    const commandHandlers = {
         'scrollToTop': function() {
             var main = document.getElementById('main');
             if (main && main.scrollIntoView) {
@@ -16,31 +16,67 @@ window.exec = (function() {
             window.history.back()
         }
     }
-    return function(direct, opts) {
-        return directHandlers[direct](opts)
+    return function(command, opts) {
+        if(typeof commandHandlers[command] === 'function') {
+            return commandHandlers[command](opts)
+        }
+        console.error('command does not exist!');
+        return console.log
     }
 })()
-window.addEventListener('load', function() {
-    function autoDisplayHeader() {
-        const main = document.getElementById('main');
-        const header = document.getElementById('app_header')
-        let PreY = main.getBoundingClientRect().y;
-        let dec = 0;
-        window.onscroll = function(e) {
-            if(dec === 0) {
-                dec = setTimeout(() => {
-                    dec = 0;
-                    if(main.getBoundingClientRect().y - PreY < -5) {
-                        header.classList.add('hide')
-                        PreY = main.getBoundingClientRect().y;
-                    }
-                    if(main.getBoundingClientRect().y - PreY > 5) {
-                        header.classList.remove('hide')
-                        PreY = main.getBoundingClientRect().y;
-                    }
-                }, 50)
-            }
+
+// 简易自定义事件
+const myEvent = {
+    cbsStore: {},
+    on: function (eventStr, cb) {
+        if(!this.cbsStore[eventStr]) {
+            this.cbsStore[eventStr] = []
+        }
+        this.cbsStore[eventStr].push(cb)
+    },
+    emit: function(eventStr, ...other) {
+        if(this.cbsStore[eventStr]) {
+            this.cbsStore[eventStr].forEach(cb => {
+                cb(...other)
+            });
+        }
+    },
+    rmListerer: function(eventStr) {
+        this.cbsStore[eventStr] = null
+    }
+}
+
+function autoDisplayHeader() {
+    const header = document.getElementById('app_header');
+    myEvent.on('scrollUp', function name(e) {
+        header.classList.remove('hide')
+    })
+    myEvent.on('scrollDown', function name(e) {
+        header.classList.add('hide')
+    })
+}
+function scrollHander() {
+    const main = document.getElementById('main');
+    let PreY = main.getBoundingClientRect().y;
+    let dec = 0;
+    window.onscroll = function(e) {
+        if(dec === 0) {
+            dec = setTimeout(() => {
+                dec = 0;
+                if(main.getBoundingClientRect().y - PreY < -5) {
+                    myEvent.emit('scrollDown', e);
+                    PreY = main.getBoundingClientRect().y;
+                }
+                if(main.getBoundingClientRect().y - PreY > 5) {
+                    myEvent.emit('scrollUp', e);
+                    PreY = main.getBoundingClientRect().y;
+                }
+            }, 50)
         }
     }
+}
+
+window.addEventListener('load', function() {
+    scrollHander()
     autoDisplayHeader()
 })
