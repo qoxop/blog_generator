d5cf38abe1552763cf92a3907751a8a9940c8e08
张集伟 <zhangjw01@mingyuanyun.com>
2019-03-21 17:58:53 +0800

    delete

--- meta.js
+++ meta.js
@@ -1,15 +1,5 @@
 const path = require('path')
 
-const menus = [
-    {text: 'HOME', url: '/index.html',  className: '', name: 'home' },
-    {text: 'CATEGORY',  url: '/category.html', className: '', name: 'category' },
-    {text: '标签', url: '/tags.html', className: '', name: 'tags' },
-    {text: '链接', url: '/link.html', className: '', name: 'link'},
-    {text: '关于我', url: '/about.html', className: '', name: 'about'}
-]
-const notePath = 'C:/work/myNotes';
-const githubPagePath = 'C:/work/qoxop.github.io'
-
 const INPUT_PATH = 'C:/work/myNotes';
 const OUTPUT_PATH = path.join(__dirname, './site');
 const REPO_PATH = 'C:/work/qoxop.github.io'
